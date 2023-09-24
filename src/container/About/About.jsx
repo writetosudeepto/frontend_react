f2cd8843a42b08b2be4d81367ddf2296a0c6ca80
writetosudeepto <writetosudeepto@gmail.com>
2023-09-25 00:17:52 +0530

    frontend-commit-1

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import "./About.scss";
 import { images } from "../../constants";
 import { urlFor, client } from "../../client";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -43,4 +43,8 @@ const About = () => {
   );
 };
 
-export default AppWrap(About, "about");
+export default AppWrap(
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
+);
