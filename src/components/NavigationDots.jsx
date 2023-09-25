441b2c7db7cf7882f5b1ee65f9be39af5a2525a5
writetosudeepto <writetosudeepto@gmail.com>
2023-09-25 18:08:31 +0530

    navbar dots open

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -6,13 +6,12 @@ const NavigationDots = ({ active }) => {
     <div className="app__navigation">
       {["home", "about", "work", "skills", "testimonial", "contact"].map(
         (item, index) => (
-          <h2>Hello</h2>
-          // <a
-          //   href={`#${item}`}
-          //   key={item + index}
-          //   className="app__navigation-dot"
-          //   style={active === item ? { backgroundColor: "#313BAC" } : {}}
-          // />
+          <a
+            href={`#${item}`}
+            key={item + index}
+            className="app__navigation-dot"
+            style={active === item ? { backgroundColor: "#313BAC" } : {}}
+          />
         )
       )}
     </div>
